b85f4355440000844564779b8adc69bbddf5a240
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-10 23:44:29 +0400

    setup page layout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
+      <nav>our navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
+      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
