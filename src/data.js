2418495b6efea094530396c157a2c5491bb7c110
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-11 01:15:59 +0400

    setup single product component

--- src/data.js
+++ src/data.js
@@ -1,36 +1,36 @@
 const products = [
   {
-    id: 'recZkNf2kwmdBcqd0',
-    name: 'accent chair',
+    id: "recZkNf2kwmdBcqd0",
+    name: "accent chair",
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681749482/react-comfy-store-products/iuYyO9RP_o_upinxq.jpg',
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*mHySsKkkYV9PPEstVg56jg.png",
   },
   {
-    id: 'recEHmzvupvT8ZONH',
-    name: 'albany sectional',
+    id: "recEHmzvupvT8ZONH",
+    name: "albany sectional",
 
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750782/react-comfy-store-products/product-2_lusrzx.jpg',
+      "https://res.cloudinary.com/rubberdesk/image/upload/h_450,c_fill,f_auto,d_space-placeholder.jpg,q_auto/v1689834793/spaces/live-uk/a6ec8e72541af/External-Private-Office-for-up-to-9-People-350-Sq-Ft-1689834792",
   },
   {
-    id: 'rec5NBwZ5zCD9nfF0',
-    name: 'albany table',
+    id: "rec5NBwZ5zCD9nfF0",
+    name: "albany table",
 
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750874/react-comfy-store-products/product-3_znpiqa.jpg',
+      "https://res.cloudinary.com/rubberdesk/image/upload/h_450,c_fill,f_auto,d_space-placeholder.jpg,q_auto/v1689834791/spaces/live-uk/a6ec8e72541af/External-Private-Office-for-up-to-9-People-350-Sq-Ft-1689834791",
   },
   {
-    id: 'recd1jIVIEChmiwhe',
-    name: 'armchair',
+    id: "recd1jIVIEChmiwhe",
+    name: "armchair",
 
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750929/react-comfy-store-products/product-4_ebl6q1.jpg',
+      "https://res.cloudinary.com/rubberdesk/image/upload/h_450,c_fill,f_auto,d_space-placeholder.jpg,q_auto/v1689834786/spaces/live-uk/a6ec8e72541af/External-Private-Office-for-up-to-9-People-350-Sq-Ft-1689834785",
   },
   {
-    id: 'recoM2MyHJGHLVi5l',
-    name: 'bar stool',
+    id: "recoM2MyHJGHLVi5l",
+    name: "bar stool",
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681751026/react-comfy-store-products/product-5_n184nu.jpg',
+      "https://res.cloudinary.com/rubberdesk/image/upload/h_450,c_fill,f_auto,d_space-placeholder.jpg,q_auto/v1689834789/spaces/live-uk/a6ec8e72541af/External-Private-Office-for-up-to-9-People-350-Sq-Ft-1689834788",
   },
 ];
 
